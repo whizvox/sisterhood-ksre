beaee68b1301b11b56413d2424ac30df324b57b6
whizvox <corneliuseanes@gmail.com>
2025-01-17 01:47:58 -0800

    Add option to build script to create a zip file

--- scripts/build.py
+++ scripts/build.py
@@ -1,6 +1,9 @@
 from rpatool import RenPyArchive
 from sishoodutil import sh_glob, relative_to_ks_path, resolve_path, update_paths
 from pathlib import Path
+from zipfile import ZipFile
+import argparse
+import sys
 
 INCLUDED_DIRS = ("bgm", "bgs", "event", "font", "gui", "logo", "sfx", "sprites", "vfx")
 
@@ -13,7 +16,7 @@ def get_project_files() -> list[Path]:
         files.append(scriptpath)
     return files
 
-def create_renpy_archive(files: list[Path]) -> RenPyArchive:
+def create_rpa(files: list[Path]) -> RenPyArchive:
     archive = RenPyArchive()
     for file in files:
         with file.open("rb") as fp:
@@ -22,11 +25,22 @@ def create_renpy_archive(files: list[Path]) -> RenPyArchive:
             archive.add(relpath, fp.read())
     return archive
 
-def main():
+def write_to_zip(files: list[Path], zip: ZipFile) -> None:
+    for file in files:
+        zip.write(str(file.absolute), str(relative_to_ks_path(file)))
+
+def main(args: dict[str, any]):
     update_paths()
     files = get_project_files()
-    archive = create_renpy_archive(files)
-    archive.save(resolve_path("scripts/build/sisterhood.rpa"))
-    
+    if args["archive"] == "rpa":
+        archive = create_rpa(files)
+        archive.save(resolve_path("scripts/build/sisterhood.rpa"))
+    elif args["archive"] == "zip":
+        with ZipFile(resolve_path("scripts/build/sisterhood.zip")) as zip:
+            for file in files:
+                zip.write(str(file.absolute), str(relative_to_ks_path(file)))
+
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(prog="build")
+    parser.add_argument("-a", "--archive", choices=["rpa", "zip"], default="rpa")
+    main(vars(parser.parse_args(sys.argv[1:])))
