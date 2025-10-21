a81dca2c940f0a9331d3dd4aea9050792eefb9bb
whizvox <corneliuseanes@gmail.com>
2025-10-21 11:38:49 -0700

    More work on chapter 28 journal

--- scripts/shimgx.py
+++ scripts/shimgx.py
@@ -244,7 +244,8 @@ JPEGS = [
 PHOTOGRAPHS = [
     # chapter 28
     ("event/planeride/planeride_bliss.jpg", "gui/journal/p01.jpg", [crop(300, 0, 1920, 1080), resize(525, 350)]),
-    ("bgs/inverness_street.jpg", "gui/journal/p02.jpg", [CompositeTransformation([(700, 0, "~sprites/hanako/hanako_emb_smile.png"), (300, 0, "sprites/hisao/hisao_cross_smile_polo.png")]), crop(150, 0, 1770, 1080), resize(525, 350)])
+    ("bgs/inverness_street.jpg", "gui/journal/p02.jpg", [CompositeTransformation([(700, 0, "~sprites/hanako/hanako_emb_smile.png"), (300, 0, "sprites/hisao/hisao_cross_smile_polo.png")]), crop(150, 0, 1770, 1080), resize(525, 350)]),
+    ("bgs/inverness_tree.jpg", "gui/journal/p03.jpg", [CompositeTransformation([(384, 0, "~sprites/lilly/lilly_basic_cheerful_cas.png"), (1074, 30, "~sprites/hanako/hanako_basic_bashful_cas.png"), (692, 30, "sprites/hisao/hisao_basic_smile_polo.png")]), crop(162, 0, 1782, 1080), resize(525, 350)])
 ]
 
 def main(args):
